2ac05f8fcc42c9db36e717733e0feb857099fb16
AKURO <shocked1370@gmail.com>
2023-06-17 09:32:58 -0500

    Added date interval (always get the current time), and created two functions: always get the hour in a 12h format and the hour as text (e.g. "Eight," "Twelve" … )

--- time.js
+++ time.js
@@ -1,7 +1,55 @@
 const timeLineOneText = document.getElementById("timeLineOneText");
+const timeLineTwoText = document.getElementById("timeLineTwoText");
 
 setInterval(() => {
   const d = new Date();
   
-  timeLineOneText.innerHTML = d;
+  function get12Hours() {
+    if (d.getHours() > 12) {
+      let newHours = d.getHours() - 12;
+      
+      if (newHours == 0) {
+        return 12;
+      } else {
+        return newHours;
+      }
+    } else {
+      if (d.getHours() == 0) {
+        return 12;
+      } else {
+        return d.getHours();
+      }
+    }
+  }
+  
+  function hourToText() {
+    if (get12Hours() == 1) {
+      return "One";
+    } else if (get12Hours() == 2) {
+      return "Two";
+    } else if (get12Hours() == 3) {
+      return "Three";
+    } else if (get12Hours() == 4) {
+      return "Four";
+    } else if (get12Hours() == 5) {
+      return "Five";
+    } else if (get12Hours() == 6) {
+      return "Six";
+    } else if (get12Hours() == 7) {
+      return "Seven";
+    } else if (get12Hours() == 8) {
+      return "Eight";
+    } else if (get12Hours() == 9) {
+      return "Nine";
+    } else if (get12Hours() == 10) {
+      return "Ten";
+    } else if (get12Hours() == 11) {
+      return "Eleven";
+    } else if (get12Hours() == 12) {
+      return "Twelve";
+    }
+  }
+  
+  timeLineOneText.innerHTML = hourToText();
+  timeLineTwoText.innerHTML = get12Hours();
 }, 1000);
